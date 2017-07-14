3eece540f778da8a5b860efc07479a84f25fae9d
keshaipad <keshaipad@gmail.com>
2017-07-14 09:41:31 +0700

    Initial Commit

---
+++ CryptoExchangers/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CryptoExchangers
+//
+//  Created by macbook pro on 14.07.17.
+//  Copyright © 2017 Qottie. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
